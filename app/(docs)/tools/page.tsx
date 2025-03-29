a7ed00c966a4bb6b200dc2cf563d7f87386cb538
Rohit kumar yadav <rohitkuyada@gmail.com>
2025-03-30 00:59:00 +0530

    Add Tools component to render ToolsCollection

---
+++ app/(docs)/tools/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ToolsCollection from './Component'
+
+const Tools = () => {
+    return (
+        <div>
+            <ToolsCollection />
+        </div>
+    )
+}
+
+export default Tools
